5affd0246532c7b386f19adf83688881fc8c00cf
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-04-11 17:05:21 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
 /* Import CTZN Bot Helper */
 const bot = require('./bot.js');
 
-const notify = function(data){
-   console.log(data);
+/* Define a handler for fetching updates */
+const notify = async function(data){
+   const updates = await bot.getNotifications(data.count);
+   console.log('you got meowd', updates);
 }
 
-/* Define a callback function */
+/* Define a main callback function */
 const main = async function(config){
    try {
        const userInfo = await bot.login(config);
