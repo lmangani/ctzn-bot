0c03878e5b4ca382323b5c303aa4f1ec62b3702b
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2021-04-11 17:58:30 +0200

    Update example.js

--- example.js
+++ example.js
@@ -1,23 +1,23 @@
 /* Import CTZN Bot Helper */
-const bot = require('./bot.js');
+const bot = require("./bot.js");
 
 /* Define a handler for fetching updates */
-const notify = async function(data){
-   const updates = await bot.getNotifications(data.count);
-   console.log('you got meowd', updates);
-}
+const notify = async function(data) {
+  const updates = await bot.getNotifications(data.count);
+  console.log("you got meowd", updates);
+};
 
 /* Define a main callback function */
-const main = async function(config){
-   try {
-       const userInfo = await bot.login(config);
-       await bot.postSelf('Bot Online at '+new Date().toISOString(),'');
-       bot.getNotification(notify);
-   }catch (e){
-       console.log(e);
-       bot.disconnect();
-       process.exit();
-   }
+const main = async function(config) {
+  try {
+    const userInfo = await bot.login(config);
+    await bot.postSelf("Bot Online at " + new Date().toISOString(), "");
+    bot.getNotification(notify);
+  } catch (e) {
+    console.log(e);
+    bot.disconnect();
+    process.exit();
+  }
 };
 
 /* Connect w/ callback */
